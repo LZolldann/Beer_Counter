11598bf886b5499c571a2dd97832f94569deb9d6
Luca <Luca.Zolldann@TUHH.de>
2020-11-09 18:36:31 +0100

    working 1000 with blink
    The binary visualization of ever 1000 beer should work stable and also the blinking show for the 1000th beer works, but there are some 'random' bugs while handling as sometimes the buttons don't react and µC doesn't enter sleep mode. Probably this is caused by unintended interrupts from turning-nob...

--- Code/AvrBuild.bat
+++ Code/AvrBuild.bat
@@ -1,2 +1,2 @@
 @ECHO OFF
-"C:\Program Files (x86)\Atmel\AVR Tools\AvrAssembler2\avrasm2.exe" -S "C:\Temp\Mikrocontroller\Code\labels.tmp" -fI -W+ie -C V2E -o "C:\Temp\Mikrocontroller\Code\Test.hex" -d "C:\Temp\Mikrocontroller\Code\Test.obj" -e "C:\Temp\Mikrocontroller\Code\Test.eep" -m "C:\Temp\Mikrocontroller\Code\Test.map" "C:\Temp\Mikrocontroller\Code\Main.asm"
+"C:\Program Files (x86)\Atmel\AVR Tools\AvrAssembler2\avrasm2.exe" -S "C:\Privat\Mikrocontroller\Code\labels.tmp" -fI -W+ie -C V2E -o "C:\Privat\Mikrocontroller\Code\Test.hex" -d "C:\Privat\Mikrocontroller\Code\Test.obj" -e "C:\Privat\Mikrocontroller\Code\Test.eep" -m "C:\Privat\Mikrocontroller\Code\Test.map" "C:\Privat\Mikrocontroller\Code\Main.asm"
